1318132ae4bac2ec723a9d92a95b1946c57f69e9
Jan Benisek <jan@smallpdf.com>
2025-06-01 14:53:04 +0200

    enabled metabase

--- projects/sqlmesh-analysis/models/slv_kassenbons.sql
+++ projects/sqlmesh-analysis/models/slv_kassenbons.sql
@@ -1,21 +1,21 @@
 
 MODEL (
-  name migros.slv_kassenbons,
+  name migros.kassenbons,
   kind FULL,
   cron '@daily',
-  grain (Datum, Kassennummer, Artikel),
+  grain (datum, kassennummer, artikel),
   -- audits (assert_positive_order_ids),
 );
 select
-	Datum,
-	Zeit,
-	Filiale,
-	Kassennummer,
-	Transaktionsnummer,
-	Artikel,
-	Menge,
-	Aktion,
-	Umsatz,
+	to_date(Datum, 'DD.MM.YYYY') as datum,
+	Zeit as zeit,
+	Filiale as filiale,
+	Kassennummer as kassennummer,
+	Transaktionsnummer as transaktionnummer,
+	Artikel as artikel,
+	Menge as menge,
+	Aktion as aktion,
+	Umsatz as umsatz,
 	(Umsatz + abs(Aktion/Menge)) as sku_price
 
 from migros.raw_kassenbons
